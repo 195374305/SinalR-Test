787991f73032ef525302b195587048464b108026
195374305 <195374305@qq.com>
2022-03-06 18:31:46 +0800

    123

--- Chat/Program.cs
+++ Chat/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddSignalR();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,7 +22,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
 app.MapRazorPages();
 app.MapHub<ChatHub>("/chatHub");
 app.Run();
